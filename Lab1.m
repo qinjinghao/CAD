e7d82e7b4ebbc3b5460f8e664b4b5d16f461fd05
qinjinghao <74455354+qinjinghao@users.noreply.github.com>
2020-11-15 11:38:32 +0800

    Add files via upload

--- Lab1.m
+++ Lab1.m
@@ -1,13 +1,24 @@
- function [] = Lab1()%mybez里面的
+ function [] = Lab1()%定义函数
 clear 
 clc
+disp('If you want to run this by a file remember to rename the file as myfile.txt and store it in the right folder')
 w=input('Press 1 for entry through keyboard or 2 for txtfile repectively-->');%可调整输入模式
 if w==1
-    [p]=input('Enter co-odinates of points within brackets ->[x1 y1 z1;...;xn yn zn] ');
     m=input('Enter no. of curves  ');
-    n=input('Enter no. of points  ');
-    cop=input('close or open 0/1 ');
-    n1=n-1;
+    fid=fopen('test.txt','w');
+    fprintf(fid,'%d\n',m);
+    for j=1:m
+        n(j,1)=input('Enter no. of points  ');
+        fprintf(fid,'%d\n',n(j,1));
+        cop(j,1)=input('close or open 0/1 ');
+        fprintf(fid,'%d\n',cop(j,1));
+        for i=1:n
+        p(i,:)=input('Enter co-odinates of points  ->[x y z] ');
+        end
+        data{j}=p;
+        fprintf(fid,'%.2f %.2f %.2f\n',p');
+    end
+    fclose(fid);
 end
 if w==2
     filename = '.\myfile.txt';
@@ -26,7 +37,8 @@ if w==2
 end
 div = 50; %曲线分段数量 (Increase this value to obtain a smoother curve）
 for j=1:m
-%     if cop(j,1)==1%open 曲线
+    %_______open 曲线
+    if cop(j,1)==1
     n1=n(j,1)-1;
     for    i=0:n1
     sigma(i+1)=factorial(n1)/(factorial(i)*factorial(n1-i));  % for calculating (x!/(y!(x-y)!)) values
@@ -41,9 +53,27 @@ for j=1:m
     end
     p=data{j};
     P=l*p;
+    end
+    %_______close 曲线
+    if cop(j,1)==0
+    for    i=0:n(j,1)
+    sigma(i+1)=factorial(n(j,1))/(factorial(i)*factorial(n(j,1)-i));  % for calculating (x!/(y!(x-y)!)) values
+    end
+    l=[];
+    UB=[];
+    for u=0:1/div:1
+        for d=1:n(j,1)+1
+        UB(d)=sigma(d)*((1-u)^(n(j,1)+1-d))*(u^(d-1));
+        end
+        l=cat(1,l,UB);                                      %catenation
+    end
+    p=data{j};p=cat(1,p,p(1,:));
+    P=l*p;
+    end
     plot3(P(:,1),P(:,2),P(:,3))
     hold on;grid on;
     plot3(p(:,1),p(:,2),p(:,3),'--r')
-    end
-
- end
+    
+end
+disp('If you use keyboard to run remember to save "text.txt" to another file or it will be rewrite')
+end
